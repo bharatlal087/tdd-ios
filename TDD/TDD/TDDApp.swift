06a152f53c94859d17e78669abf4d0fb543143fb
Bharat Lal <bharatlal087@gmail.com>
2024-12-28 10:11:02 +0530

    initial commit

---
+++ TDD/TDD/TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TDDApp.swift
+//  TDD
+//
+//  Created by Bharat Lal on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
